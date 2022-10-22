79eb7ad9acda0cc68a69947d8ee817eb3d31ffec
Tsvika S <tsvikas@gmail.com>
2022-10-22 03:46:44 +0300

    bugfix: test mode always send keyboard

--- main.py
+++ main.py
@@ -103,19 +103,18 @@ def add_player(
 
 def send_game_views(bot: telepot.Bot, chat_game: ChatGame, keyboard: bool = False):
     if chat_game.test_mode:
+        # send only once
         send_game_view(None, chat_game.admin, bot, chat_game)
-        chat_game.current_action = ""
-        send_keyboard(bot, chat_game.chat_id, KeyboardType.ACTION)
-        return
-
-    next_player = hanabi.get_active_player_name(chat_game.game)
-    next_user_id = chat_game.player_to_user[next_player]
-    send_game_view(next_player, next_user_id, bot, chat_game)
-
-    for name, user_id in chat_game.player_to_user.items():
-        if name != next_player:
-            send_game_view(name, user_id, bot, chat_game)
-
+    else:
+        # first player
+        next_player = hanabi.get_active_player_name(chat_game.game)
+        next_user_id = chat_game.player_to_user[next_player]
+        send_game_view(next_player, next_user_id, bot, chat_game)
+        # other players
+        for name, user_id in chat_game.player_to_user.items():
+            if name != next_player:
+                send_game_view(name, user_id, bot, chat_game)
+    # now send keyboard
     if keyboard:
         chat_game.current_action = ""
         send_keyboard(bot, chat_game.chat_id, KeyboardType.ACTION)
